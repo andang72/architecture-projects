bef6108e7a286ad266d6d48a6d9ac82fb4b7d5f0
donghyuck <andang.son@gmail.com>
2017-03-07 00:26:43 +0900

    Signed-off-by: donghyuck <andang.son@gmail.com>

---
+++ architecture-community/src/main/java/architecture/community/web/util/CommunityConstants.java
@@ -0,0 +1,10 @@
+package architecture.community.web.util;
+
+public class CommunityConstants {
+	
+	/** SECURITY PROPERTY KEY */
+    public static final String SECURITY_AUTHENTICATION_ENCODING_ALGORITHM_PROP_NAME = "security.authentication.encoding.algorithm";
+    public static final String SECURITY_AUTHENTICATION_ENCODING_SALT_PROP_NAME = "security.authentication.encoding.salt";
+    public static final String SECURITY_AUTHENTICATION_AUTHORITY_PROP_NAME = "security.authentication.authority";
+    
+}
